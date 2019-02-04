af11b740b79b30a5a23089bc8fa1aee70aa0dddb
github username <github email address>
2019-02-04 14:36:55 -0600

    add associations and validations to Food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,9 @@
 class Food < ApplicationRecord
+  has_many :meal_compositions
+  has_many :meals, through: :meal_compositions
+  has_many :food_compositions
+  has_many :macronutrients, through: :food_compositions
+
+  validates :name, presence: true
+  validates :name, uniquesness: true 
 end
