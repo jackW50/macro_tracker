95f79d2553e7dd8d739a5b1eab20338f5feb60bd
github username <github email address>
2019-02-04 15:21:10 -0600

    add class query methods to MealComposition model

--- app/models/meal_composition.rb
+++ app/models/meal_composition.rb
@@ -36,4 +36,24 @@ class MealComposition < ApplicationRecord
     self.grams_of_macro_for_each_food_in_meal(meal, macronutrient).sum
   end
 
+  def self.find_grams_and_servings(meal)
+    self.composition(meal).joins(food: :food_compositions).pluck('food_compositions.grams', 'meal_compositions.servings')
+  end
+
+  def self.collect_total_grams_of_each_food_in_meal(meal)
+    self.find_grams_and_servings(meal).collect { |arr| arr[0] * arr[1] }
+  end
+
+  def self.total_grams_in_meal(meal)
+    self.collect_total_grams_of_each_food_in_meal(meal).sum
+  end
+
+  def self.percent_of_macro_calories(meal, macronutrient)
+    (self.total_calorie_from_macro_in_meal(meal, macronutrient).to_f / self.total_calories_in_meal(meal) * 100).round(2)
+  end
+
+  def self.percent_of_macro_grams(meal, macronutrient)
+    (self.total_grams_of_macro_in_meal(meal, macronutrient).to_f / self.total_grams_in_meal(meal) * 100).round(2)
+  end
+  
 end
