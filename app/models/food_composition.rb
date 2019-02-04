fdd66bab7b7353e35bb33ed652980b5e489e545d
github username <github email address>
2019-02-04 15:25:19 -0600

    add scope and class query methods to FoodComposition model

--- app/models/food_composition.rb
+++ app/models/food_composition.rb
@@ -2,5 +2,16 @@ class FoodComposition < ApplicationRecord
   belongs_to :food
   belongs_to :macronutrient
 
-  validates :macronutrient_grams, presence: true 
+  validates :macronutrient_grams, presence: true
+
+  scope :total_grams_of_macro, ->(food, macronutrient) { where('food_compositions.food_id = ? AND food_compositions.macronutrient_id = ?', food.id, macronutrient.id).pluck(:grams).first }
+
+  def self.total_calories_from_macro(food, macronutrient)
+    total_grams_of_macro(food, macronutrient) * macronutrient.calories_per_gram
+  end
+
+  def self.total_calories(food)
+    food.macronutrients.collect {|macro| self.total_calories_from_macro(food, macro) }.sum
+  end
+  
 end
