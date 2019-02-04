2cb2fa6ae3794be0cf327ee5b73ba37d27d68ffa
github username <github email address>
2019-02-04 15:10:13 -0600

    add class query method to Meal model

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -5,4 +5,8 @@ class Meal < ApplicationRecord
 
   scope :today, -> { where("time > :begin_day AND time < :end_day)", begin_day: DateTime.now.midnight, end_day: DateTime.now.midnight + 1)}
   scope :chronological, -> { order(time: :asc) }
+
+  def self.find_user_meals_today(user)
+    self.today.where(user: user.id)
+  end
 end
