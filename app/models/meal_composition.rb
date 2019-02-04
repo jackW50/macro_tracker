e463df223f48a9f3f8540a35cd2a508febe45524
github username <github email address>
2019-02-04 15:13:39 -0600

    add scope and query methods to MealComposition model

--- app/models/meal_composition.rb
+++ app/models/meal_composition.rb
@@ -2,5 +2,17 @@ class MealComposition < ApplicationRecord
   belongs_to :meal
   belongs_to :food
 
-  validates :food_servings, numericality: true 
+  validates :food_servings, numericality: true
+
+  scope :composition, ->(meal) { where('meal_compositions.meal_id = ?', meal.id) }
+
+  def self.calories_for_each_food(meal)
+    self.composition(meal).collect do |meal_comp|
+      FoodComposition.total_calories(meal_comp.food) * meal_comp.servings
+    end
+  end
+
+  def self.total_calories_in_meal(meal)
+    self.calories_for_each_food(meal).sum
+  end
 end
