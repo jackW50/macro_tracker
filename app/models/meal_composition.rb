5d8caa3d205cda2865f28d9377aff07ea37b9b23
github username <github email address>
2019-02-04 15:16:10 -0600

    add more class query methods to MealComposition class

--- app/models/meal_composition.rb
+++ app/models/meal_composition.rb
@@ -15,4 +15,25 @@ class MealComposition < ApplicationRecord
   def self.total_calories_in_meal(meal)
     self.calories_for_each_food(meal).sum
   end
+
+  def self.calories_from_macro_for_each_food_in_meal(meal, macronutrient)
+    self.composition(meal).collect do |meal_comp|
+      FoodComposition.total_calories_from_macro(meal_comp.food, macronutrient) * meal_comp.servings
+    end
+  end
+
+  def self.total_calorie_from_macro_in_meal(meal, macronutrient)
+    self.calories_from_macro_for_each_food_in_meal(meal, macronutrient).sum
+  end
+
+  def self.grams_of_macro_for_each_food_in_meal(meal, macronutrient)
+    self.composition(meal).collect do |meal_comp|
+      FoodComposition.total_grams_of_macro(meal_comp.food, macronutrient) * meal_comp.servings
+    end
+  end
+
+  def self.total_grams_of_macro_in_meal(meal, macronutrient)
+    self.grams_of_macro_for_each_food_in_meal(meal, macronutrient).sum
+  end
+
 end
