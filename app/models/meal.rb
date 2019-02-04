b885f67e3219488a728ed076105f61f602e4241a
github username <github email address>
2019-02-04 15:08:28 -0600

    add scopes to Meal model

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,7 +1,8 @@
 class Meal < ApplicationRecord
-  belongs_to :user, optional: true 
+  belongs_to :user, optional: true
   has_many :meal_compositions
   has_many :foods, through: :meal_compositions
 
-
+  scope :today, -> { where("time > :begin_day AND time < :end_day)", begin_day: DateTime.now.midnight, end_day: DateTime.now.midnight + 1)}
+  scope :chronological, -> { order(time: :asc) }
 end
