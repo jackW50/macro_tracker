c17a908612ec62c3a8ce9c0479fbbf000395fa58
github username <github email address>
2019-02-04 16:12:18 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#welcome'
+
+  resources :users, only: [:new, :show, :create, :edit, :update] do
+    resources :meals, :foods
+  end
+
+  resources :sessions, only: [:new, :create, :destroy]
+
 end
