9151a59d326750f0f464cf737a91955f95cd86d4
github username <github email address>
2019-02-05 14:59:54 -0600

    add show page an show action for meals controller

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,31 @@
 class MealsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+    raise params.inspect 
+    @meal = Meal.find(params[:meal_id])
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
