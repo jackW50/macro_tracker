389208a1a027eced1d4535642eda29bc9b88a02b
github username <github email address>
2019-02-05 12:39:17 -0600

    change nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'sessions#welcome'
 
-  resources :users, only: [:new, :show, :create, :edit, :update] do
-    resources :meals, :foods
-  end
+  resources :users, only: [:new, :show, :create, :edit, :update]
+
+  resources :meals do
+    resources :foods
+  end 
 
   resources :sessions, only: [:new, :create, :destroy] do
     collection do
       get 'welcome'
     end
-  end 
+  end
 
 end
