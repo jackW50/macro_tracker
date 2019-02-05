27e8d9c48f9afb7ee2dddbb94a5a8d82d706849d
github username <github email address>
2019-02-05 13:09:27 -0600

    users contoller actions new, create, show, edit, update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+    raise params.inspect
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
 end
