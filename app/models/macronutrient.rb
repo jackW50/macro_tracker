a649cd553394e422be6a4dd8fe70938dac39c5d2
github username <github email address>
2019-02-05 15:20:10 -0600

    add instance method to Macronutrient model and updated display for food show page

--- app/models/macronutrient.rb
+++ app/models/macronutrient.rb
@@ -1,5 +1,12 @@
 class Macronutrient < ApplicationRecord
   has_many :food_compositions
   has_many :foods, through: :food_compositions
-  
+
+  def grams(food)
+    FoodComposition.total_grams_of_macro(food, self)
+  end
+
+  def calories(food)
+    FoodComposition.total_calories_from_macro(food, self)
+  end 
 end
