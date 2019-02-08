055b4c2cd2c00ad20234583337819ecd34b954c4
github username <github email address>
2019-02-08 15:43:24 -0600

    created update action

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,6 +3,9 @@ class FoodsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
   def show
     @food = Food.find(params[:id])
     @meal = Meal.find(params[:meal_id]) if params[:meal_id].present?
@@ -11,5 +14,19 @@ class FoodsController < ApplicationController
   def edit
     @food = Food.find(params[:id])
     @meal = Meal.find(params[:meal_id]) if params[:meal_id].present?
+  end
+
+  def update
+    #raise params.inspect
+    meal = Meal.find(params[:meal_id])
+    food = Food.find(params[:id])
+    food.macronutrient_ids = params[:food][:macronutrient_ids]
+    redirect_to meal_food_path(meal, food)
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :macronutrient_ids)
   end
 end
