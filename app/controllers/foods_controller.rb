78db89e0590fd149026e0fdb49b31bbc39fe080d
github username <github email address>
2019-02-08 13:15:31 -0600

    add edit link to show page and create foods edit page

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,13 @@
 class FoodsController < ApplicationController
 
+  def index
+  end 
+
   def show
     @food = Food.find(params[:id])
-    
+    @meal = Meal.find(params[:meal_id]) if params[:meal_id].present?
+  end
+
+  def edit
   end
 end
