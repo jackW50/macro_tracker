922f031eea57852f437b078d8a7d40b6c45934dc
github username <github email address>
2019-02-11 12:21:37 -0600

    refactor food macronutrients partial

--- app/helpers/foods_helper.rb
+++ app/helpers/foods_helper.rb
@@ -1,2 +1,27 @@
 module FoodsHelper
+
+  def errors_present?(food)
+    if food.errors[:food_compositions].present?
+      content_tag :p, class: "field_with_errors" do
+        yield
+      end
+    else
+      content_tag :p do
+        yield
+      end
+    end
+  end
+
+  def checked?(food, macronutrient)
+    food.macronutrient_ids.include?(macronutrient.id)
+  end
+
+  def food_grams?(food, macronutrient)
+    if food.grams_of_macronutrient(macronutrient) == 0
+      nil
+    else
+      food.grams_of_macronutrient(macronutrient)
+    end
+  end
+
 end
