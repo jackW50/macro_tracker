86874bd09094271a7bbd138e94b53890dac92812
github username <github email address>
2019-02-12 10:37:23 -0600

    add column password digest to user, bcrypt gem to gemfile, and has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :username, uniqueness: true
 
+  has_secure_password
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_04_201926) do
+ActiveRecord::Schema.define(version: 2019_02_12_162707) do
 
   create_table "food_compositions", force: :cascade do |t|
     t.integer "macronutrient_grams"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2019_02_04_201926) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
