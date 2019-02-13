61a93faaa5073ec8202a32440afccc9dc4485f01
github username <github email address>
2019-02-13 13:06:40 -0600

    add authorize check, and before action methods to meals controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,49 +5,39 @@ class FoodsController < ApplicationController
   before_action :find_food, only: [:show, :edit, :update, :destroy]
 
   def index
-    #@meal = Meal.find(params[:meal_id])
   end
 
   def new
-    #@meal = Meal.find(params[:meal_id])
     @food = Food.new
-    #@macronutrients = Macronutrient.all
   end
 
   def create
     @meal = Meal.find(params[:meal_id])
     @food = Food.new(food_params)
     associate_to_meal(@meal, @food, params[:food_servings])
+    
     if @food.save
       redirect_to meal_path(@meal)
     else
-      #@macronutrients = Macronutrient.all
       render :new
     end
   end
 
   def show
-    #@food = Food.find(params[:id])
   end
 
   def edit
-    #@macronutrients = Macronutrient.all
-    #@food = Food.find(params[:id])
   end
 
   def update
-    #@food = Food.find(params[:id])
-
     if @food.update(food_params)
       redirect_to food_path(@food)
     else
-      #@macronutrients = Macronutrient.all
       render :edit
     end
   end
 
   def destroy
-    #@food = Food.find(params[:id])
     @food.food_compositions.each do |f|
       f.destroy
     end
@@ -55,7 +45,7 @@ class FoodsController < ApplicationController
       m.destroy
     end
     @food.destroy
-    redirect_to user_path(current_user), notice: "You just deleted #{food.name} from database."
+    redirect_to user_path(current_user), notice: "You just deleted #{@food.name} from database."
   end
 
   private
