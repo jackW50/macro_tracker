6d98e130505e29afdcecf671b9c3ef8c56ed73a7
github username <github email address>
2019-04-03 10:01:13 -0500

    correct errors on service object and its usage in the foods controller create action

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -22,6 +22,7 @@ class FoodsController < ApplicationController
         format.json { render json: {
           food: @food,
           food_servings: params[:food_servings],
+          table_data: NewTableDataService.new.call_meal(@meal)
            }
          }
       end
--- app/services/new_table_data_service.rb
+++ app/services/new_table_data_service.rb
@@ -1,7 +1,20 @@
 class NewTableDataService
-  
-  def call_meal(meal)
 
+  def initialize
+    @carbohydrate =  Macronutrient.find_by(category: "carbohydrate")
+    @protein = Macronutrient.find_by(category: "protein")
+    @fat = Macronutrient.find_by(category: "fat")
+  end
+
+  def call_meal(meal)
+    {
+      carbohydrate: meal.grams_of_macronutrient(@carbohydrate),
+      percent_calories_of_carbohydrate: meal.percent_calories_macronutrient(@carbohydrate),
+      protein: meal.grams_of_macronutrient(@protein),
+      percent_calories_of_protein: meal.percent_calories_macronutrient(@protein),
+      fat: meal.grams_of_macronutrient(@fat),
+      percent_calories_of_fat: meal.percent_calories_macronutrient(@fat)
+    }
   end
 
 end
