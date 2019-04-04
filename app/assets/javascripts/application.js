0717dc16da40dd76da6ede291067424df1bfb5d6
github username <github email address>
2019-04-04 11:41:24 -0500

    add a macronutrient table class to JavaScript to use in my AJAX responses to update the table data

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -53,6 +53,25 @@ class Food {
   }
 }
 
+class macronutrientTable {
+  constructor(carbohydrateGrams, carbohydratePercent, proteinGrams, proteinPercent, fatGrams, fatPercent) {
+    this.carbohydrateGrams = carbohydrateGrams,
+    this.carbohydratePercent = carbohydratePercent,
+    this.proteinGrams = proteinGrams,
+    this.proteinPercent = proteinPercent,
+    this.fatGrams = fatGrams,
+    this.fatPercent = fatPercent
+  }
+  updateTable() {
+    $('tr td')[1].innerHTML = this.carbohydrateGrams
+    $('tr td')[2].innerHTML = this.carbohydratePercent
+    $('tr td')[4].innerHTML = this.proteinGrams
+    $('tr td')[5].innerHTML = this.proteinPercent
+    $('tr td')[7].innerHTML = this.fatGrams
+    $('tr td')[8].innerHTML = this.fatPercent
+  }
+}
+
 function resetSubmit() {
   $('#add-food-form input[type="submit"]').prop('disabled', false);
 }
