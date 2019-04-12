ea9c9bf980875f279f583e1b183cac3e8f896a07
github username <github email address>
2019-04-12 09:58:57 -0500

    add a AJAX post request on submit of the new meal form on the user show page

--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -1,8 +1,12 @@
 class MealSerializer < ActiveModel::Serializer
-  attributes :id, :time
+  attributes :id, :time, :todays_meal?
   has_many :meal_compositions
 
   def time
     time = self.object.time.strftime('%A, %b %d, @ %l:%M %p')
+  end
+
+  def todays_meal?
+    #true or false if it is a meal for today
   end
 end
