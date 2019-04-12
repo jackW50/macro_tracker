876f5355574befff322ac5db804901fa426384b8
github username <github email address>
2019-04-12 12:10:30 -0500

    add a new method to return table data from todays meals

--- app/services/new_table_data_service.rb
+++ app/services/new_table_data_service.rb
@@ -17,7 +17,15 @@ class NewTableDataService
     }
   end
 
-  def call_todays_meals
-  end 
+  def call_todays_meals(user)
+    {
+      carbohydrate: Meal.todays_macronutrient_total(user, @carbohydrate),
+      percent_calories_of_carbohydrate: Meal.todays_percent_calories_of_macro(user, @carbohydrate),
+      protein: Meal.todays_macronutrient_total(user, @protein),
+      percent_calories_of_protein: Meal.todays_percent_calories_of_macro(user, @protein),
+      fat: Meal.todays_macronutrient_total(user, @fat),
+      percent_calories_of_fat: Meal.todays_percent_calories_of_macro(user, @fat)
+    }
+  end
 
 end
