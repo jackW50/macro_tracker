11a8938477333cfcbe51ce4882e763a9ffdd3d9b
github username <github email address>
2019-04-12 14:46:58 -0500

    include a meta tag to include table data if meal is a meal for the current day

--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -1,5 +1,5 @@
 class MealSerializer < ActiveModel::Serializer
-  attributes :id, :time, :todays_meal?, :calories 
+  attributes :id, :time, :todays_meal?, :calories
   has_many :meal_compositions
 
   def time
@@ -7,10 +7,11 @@ class MealSerializer < ActiveModel::Serializer
   end
 
   def todays_meal?
-    true
+    self.object.time >= DateTime.now.midnight && self.object.time <= DateTime.now.midnight + 1
     #true or false if it is a meal for today
   end
 
   def calories
+    self.object.calories
   end
 end
